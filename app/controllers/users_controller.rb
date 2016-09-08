30d9de0a332d91c1ca6443397fa33c6edfc9c918
masao <s13310040nm@tiu.ac.jp>
2016-09-08 12:17:50 +0000

    pro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
    if @user.update(user_params)
      # 保存に成功した場合はトップページへリダイレクト
-     redirect_to root_path , notice: 'プロフィールを更新しました'
+     redirect_to user_path(@user) , notice: 'プロフィールを更新しました'
    else
      # 保存に失敗した場合は編集画面へ戻す
      render 'edit'
