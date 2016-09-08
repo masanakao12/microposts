0436ca270d01ec31be1a50e8700e233e92f8aa8b
masao <s13310040nm@tiu.ac.jp>
2016-09-08 06:39:11 +0000

    Profile be

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+    def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user 
+    else
+      render 'new'
+    end
+  end
   
   
   def edit
